507aaa24499ef978bdfa0c0a45210f178ee02f91
Fouz <syedfouz@outlook.com>
2023-10-03 23:47:42 +0530

    Validation fix

--- src/main/java/com/example/example18/validations/FieldsValueMatchValidator.java
+++ src/main/java/com/example/example18/validations/FieldsValueMatchValidator.java
@@ -5,23 +5,28 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.BeanWrapperImpl;
 
-public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValueMatch,Object> {
+public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValueMatch, Object> {
     private String field;
     private String fieldMatch;
+
     @Override
     public void initialize(FieldsValueMatch constraintAnnotation) {
-        this.field= constraintAnnotation.field();
-        this.fieldMatch= constraintAnnotation.fieldMatch();
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
     }
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
-        Object fieldValue= new BeanWrapperImpl(value).getPropertyValue(field);
-        Object fieldMatchValue= new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
-        if (fieldValue!=null){
-            return fieldValue.equals(fieldMatchValue);
-        }else {
-            return fieldMatchValue==null;
+        Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
+        if (fieldValue != null) {
+            if (fieldValue.toString().startsWith("$2a")) {
+                return true;
+            } else {
+                return fieldValue.equals(fieldMatchValue);
+            }
+        } else {
+            return fieldMatchValue == null;
         }
     }
 }
